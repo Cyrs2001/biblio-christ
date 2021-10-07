79b04a30df3291635cddea884b4e6820b57a89d3
Cyrs2001 <90917888+Cyrs2001@users.noreply.github.com>
2021-10-07 23:37:07 +0100

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/Biblio', {
+mongoose.connect('mongodb+srv://Cyrs2001:CYRsewa@biblio.rqyq6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
     // useFindAndModify: false,
